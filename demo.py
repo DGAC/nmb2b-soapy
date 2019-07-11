deaa072fdc550e5defed20b5132b4f3187b4bbde
RENAUD Maxence DGAC/CRNA-N <maxence.renaud@aviation-civile.gouv.fr>
2019-07-11 13:17:07 +0200

    Ajout de la doc du NM

--- demo.py
+++ demo.py
@@ -1,16 +1,15 @@
 from manager import Manager
-from utils import sendTime
 
 test_manager = Manager()
 test_manager.show_available_services()
 test_manager.show_operations_of_service(service_name='FlightManagementService')
 
-# test = test_manager.queryFlightsByAirspace(
-#     airspace="LFFFUZ",
-#    	dataset='OPERATIONAL',
-#    	includeProposalFlights=False,
-#    	includeForecastFlights=True,
-#    	trafficType='LOAD',
-#    	trafficWindow={'wef': "2019-07-11 06:30", 'unt': "2019-07-11 07:00"}
-# )
-# print(test)
+test = test_manager.queryFlightsByAirspace(
+    airspace="LFFFUZ",
+   	dataset='OPERATIONAL',
+   	includeProposalFlights=False,
+   	includeForecastFlights=True,
+   	trafficType='LOAD',
+   	trafficWindow={'wef': "2019-07-11 06:30", 'unt': "2019-07-11 07:00"}
+)
+print(test)
