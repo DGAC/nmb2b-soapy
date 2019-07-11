7a737fdebbd374ec5df4987f2552faf6ea82093e
KrazyMax <maxence.renaud@gmail.com>
2019-07-11 09:56:12 +0200

    update

--- manager.py
+++ manager.py
@@ -8,7 +8,7 @@ from zeep.transports import Transport
 
 from shared_vars import (B2B_PROXY, DEFAULT_B2B_VERSION, DEFAULT_DATASET,
                          WSDL_PROXY, get_dataset)
-
+import utils
 
 
 class Manager():
@@ -18,6 +18,8 @@ class Manager():
         dataset=DEFAULT_DATASET, *args, **kwargs):
         
         self.wanted_services = wanted_services
+        self.available_services = []
+        self.available_operations = {}
         self.dataset = get_dataset(dataset)
         self.session = Session()
         self.session.auth = HTTPBasicAuth(B2B_PROXY['key'], B2B_PROXY['secret'])        
@@ -29,21 +31,33 @@ class Manager():
             'transport': self.transport
         }
     
-    def show_services(self):
+    def set_available_services(self):
+        self.available_services = []
         client = Client(**self.conf)
-        print("Les services disponibles via {} sont : ".format(self.wanted_services)),
         for service in client.wsdl.services.values():
-            print(' * ', service.__str__().split()[1])
+            self.available_services.append(service.__str__().split()[1])
+
+    def show_available_services(self):
+        if not self.available_services:
+            self.set_available_services()
+        print("Les services disponibles via {} sont : ".format(self.wanted_services)),
+        for service in self.available_services:
+            print('  * ', service)
     
-    def show_operations_of_service(self, service_name):
+    def set_operations_of_service(self, service_name):
+        if not self.available_services:
+            self.set_available_services()
+        if not service_name in self.available_services:
+            print(f"Le service {service_name} n'est pas disponible.")
         client = Client(**self.conf, service_name=service_name)
         operations = [op for op in client.service.__dir__() if not op.startswith('__')]
+        self
         print(f"Les opérations disponibles pour {service_name} sont : ")
         for op in operations:
             print(' * ', op)
     
     def queryFlightsByAirspace(
-        self, airspace, sendTime, dataset, trafficType,
+        self, airspace, sendTime=utils.sendTime(), dataset, trafficType,
         includeProposalFlights, includeForecastFlights, 
         trafficWindow):
         client = Client(**self.conf, service_name='FlightManagementService')
